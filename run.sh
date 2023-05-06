2a24308665de875a9fe93014378d45f854af3682
kamalesh-pathy-va <asokankamalesh@gmail.com>
2023-05-06 14:20:30 +0530

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+cd /home/kamaleshpathy/Downloads/final_test/
+git pull origin main
 python3 /home/kamaleshpathy/Downloads/final_test/server/serve.py &
 python3 /home/kamaleshpathy/Downloads/final_test/main.py
+
